00985c6018f8480fed5b2024a8e1977c1f80dfdf
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-07-25 16:23:56 +0200

    importer des données de test à partir de "mocks"

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,9 +2,12 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+ import "@testing-library/jest-dom";
+ import { fireEvent, screen, waitFor } from "@testing-library/dom";
+ import { ROUTES_PATH } from "../constants/routes.js";
+ import { localStorageMock } from "../__mocks__/localStorage.js";
+ import mockStore from "../__mocks__/store.js";
+ import router from "../app/Router.js";
 
 
 describe("Given I am connected as an employee", () => {
