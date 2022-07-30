e9414b015d28785900479b1722121a8683b9441e
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-07-30 15:43:19 +0200

    Completer le test Bills.js

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -9,6 +9,22 @@
  import mockStore from "../__mocks__/store.js";
  import router from "../app/Router.js";
 
+ jest.mock("../app/Store", () => mockStore);
+
+ const setup = async () => {
+   Object.defineProperty(window, "localStorage", { value: localStorageMock });
+   localStorage.setItem(
+     "user",
+     JSON.stringify({ type: "Employee", email: "a@a" })
+   );
+   const root = document.createElement("div");
+   root.setAttribute("id", "root");
+   document.body.append(root);
+   router();
+   return root;
+ };
+
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -16,6 +32,21 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+      
+      // Choose a file
+      fireEvent["change"](fileInputEl, {
+        target: {
+          ...fileInputEl,
+          files: [file],
+        },
+      });
+      await waitFor(() => process.nextTick);
+
+      // Assertions
+      expect(bills.create).not.toBeCalled();
+      expect(window.location.hash).toBe(ROUTES_PATH.NewBill);
+
+      jest.restoreAllMocks();
     })
   })
 })
--- Billed-app-FR-Front-main/src/containers/Bills.js
+++ Billed-app-FR-Front-main/src/containers/Bills.js
@@ -7,14 +7,10 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
-    const buttonNewBill = document.querySelector(
-      `button[data-testid="btn-new-bill"]`,
-    )
-    if (buttonNewBill)
-      buttonNewBill.addEventListener('click', this.handleClickNewBill)
+    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
+    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
-    if (iconEye)
-      iconEye.forEach((icon) => {
+    if (iconEye) iconEye.forEach((icon) => {
         icon.addEventListener('click', () => this.handleClickIconEye(icon))
       })
     new Logout({ document, localStorage, onNavigate })
@@ -27,11 +23,7 @@ export default class {
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute('data-bill-url')
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
-    $('#modaleFile')
-      .find('.modal-body')
-      .html(
-        `<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`,
-      )
+    $('#modaleFile').find('.modal-body').html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
     $('#modaleFile').modal('show')
   }
 
@@ -47,7 +39,7 @@ export default class {
               try {
                 return {
                   ...doc,
-                  date: formatDate(doc.date),
+                  date: doc.date,
                   status: formatStatus(doc.status),
                 }
               } catch (e) {
@@ -56,11 +48,12 @@ export default class {
                 console.log(e, 'for', doc)
                 return {
                   ...doc,
-                  date: doc.date,
+                  date: formatDate(doc.date),
                   status: formatStatus(doc.status),
                 }
               }
             })
+          console.log(bills)  
           console.log('length', bills.length)
           return bills
         })
